9265786ac5f2645a923a50ce8f84ea8233937797
enricoavagliano-collab <enrico.avagliano@gmail.com>
2026-09-09 21:43:25 +0000

    via miniatura lista blog

--- app/(site)/blog/page.tsx
+++ app/(site)/blog/page.tsx
@@ -47,11 +47,6 @@ export default async function BlogIndex({
       <div className="logbook">
         {list.map((a) => (
           <Link href={`/blog/${a.slug}`} key={a.slug} className="log-entry">
-            {a.imageUrl ? (
-              <img src={a.imageUrl} alt="" className="thumb" />
-            ) : (
-              <span className="thumb" aria-hidden="true" />
-            )}
             <span className="date">{formatItDate(a.date)}</span>
             <span className="title">{a.title}</span>
             <span className="cat">{a.category}</span>
